25432e94ccdf64182c5c9670d49e0d3723bea35b
RoiDev12 <77006375+RoiDev12@users.noreply.github.com>
2022-02-11 14:49:53 +0200

    fix

--- code/src/App.jsx
+++ code/src/App.jsx
@@ -5,8 +5,6 @@ function App() {
   return (
   <>
   <a href="https://slippi.gg/downloads" target="_blank"><button>Download Slippi</button></a>
-  <a href="https://vimm.net/vault/7818" target="_blank"><button>Download Melee ISO</button></a>
-  <a href="https://slippi.gg/downloads" target="_blank"><button>Download Slippi</button></a>
   <a href="https://discord.gg/S2hhkSSGEN" target="_blank"><button>Join our Discord server</button></a>
   <h1>Melee Israel leaderboard</h1>
   <div class="container">
